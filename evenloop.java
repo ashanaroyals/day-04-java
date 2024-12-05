104fdd129f2631a44baad073460dd5b8b83d66d0
ashanaroyals <kgashalatha3@gmail.com>
2024-12-05 13:32:02 +0530

    Add files via upload

---
+++ evenloop.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class evenloop
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int i=sc.nextInt();
+        for(i=0;i<=10;i+=2)
+        {
+            System.out.println(i);
+        }
+    }
+}
